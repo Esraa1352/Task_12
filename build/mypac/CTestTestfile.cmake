7d0ce543ab62b09a53373e3d868fcea678c48ad1
Gehad Attia <gehadmohammedattia@gmail.com>
2024-09-11 21:35:59 +0300

    Initial commit of workspace

---
+++ build/mypac/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/gehadatia/catkin_ws/src/mypac
+# Build directory: /home/gehadatia/catkin_ws/build/mypac
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
